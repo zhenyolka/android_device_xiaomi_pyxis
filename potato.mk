526dcab725ca2d2fdb02a28aed4ca018865fe848
zhenyolka <zhenyolka@gmail.com>
2021-08-01 10:04:28 +0300

    Move to POSP

--- potato.mk
+++ potato.mk
@@ -6,11 +6,11 @@
 
 $(call inherit-product, device/xiaomi/pyxis/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit from Potato vendor
+$(call inherit-product, vendor/potato/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_pyxis
+PRODUCT_NAME := potato_pyxis
 PRODUCT_DEVICE := pyxis
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi 9 Lite
